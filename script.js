c023a6cc950093ff09837e9719911892a83f2db0
Ben <ben@bookmac.coffee>
2024-09-16 18:39:14 +0200

    few changes not much

--- script.js
+++ script.js
@@ -17,6 +17,14 @@ const commands = {
     },
     hello() {
         term.echo(greetings);
+    },
+    projects() {
+
+    },
+    connect(){
+        $(document).ready(function() {
+            $('.term').append('<a href="github.com">Github</a>');
+        });
     }
 
 };
@@ -25,7 +33,7 @@ const commands = {
 const greetings='\r\n                            )      (      (\r\n                           (        ))     )\r\n                    )       )      \/\/     (\r\n               _   (        __    (     ~->>\r\n        ,-----\' |__,_~~___<\'__`)-~__--__-~->> <\r\n        | \/\/  : | -__   ~__ o)____)),__ - \'> >-  >\r\n        | \/\/  : |- \\_ \\ -\\_\\ -\\ \\ \\ ~\\_  \\ ->> - ,  >>\r\n        | \/\/  : |_~_\\ -\\__\\ \\~\'\\ \\ \\, \\__ . -<-  >>\r\n        `-----._| `  -__`-- - ~~ -- ` --~> >\r\n         _\/___\\_    \/\/)_`\/\/  | ||]\r\n   _____[_______]_[~~-_ (.L_\/  ||\r\n  [____________________]\' `\\_,\/\'\/\r\n    ||| \/          |||  ,___,\'.\/\r\n    ||| \\          |||,\'______|\r\n    ||| \/          \/|| I==||\r\n    ||| \\       __\/_||  __||__\r\n-----||-\/------`-._\/||-o--o---o---\r\n  ~~~~~\'\r\nFor help type \'help\' in the terminal :)'
 
 function prompt() {
-    return (`ben@host ~ % `);
+    return (`guest@127.0.0.1 ~ % `);
 }
 
 //initalize terminal and settings
@@ -38,8 +46,8 @@ const term = $('.term').terminal(commands, {
 });
 
 //command list after help
-//const help = "\n \n clear - clear terminal output\n hello - welcome screen\n echo - print whats after echo \n help - you know what this does \n time - the time \n about - few things about me :) \n";
-const help = '\n\n general:\n hello - welcome screen\n clear - clear terminal output\n echo - print whats after echo\n help - you know what this does\n time - the time\n switch_modes - switch from darkmode to lightmode\n\n personal\n about - few things about me :)\n projects - my projects\n contact - how to contact me\n'
+const help = '\n\n general:\n hello - welcome screen\n clear - clear terminal output\n echo - print whats after echo\n help - you know what this does\n time - the time\n\n personal\n about - few things about me :)\n projects - my projects\n contact - how to contact me\n'
+
 // make the text clickable
 document.getElementById('about').addEventListener("click", function() {
     const command = $(this).text().trim();
@@ -51,7 +59,13 @@ document.getElementById('connect').addEventListener("click", function() {
     term.exec(command, {typing:true})
 });
 
-document.getElementById('switch').addEventListener("click", function() {
+document.getElementById('help').addEventListener("click", function() {
     const command = $(this).text().trim();
     term.exec(command, {typing:true})
 });
+
+document.getElementById('projects').addEventListener("click", function() {
+    const command = $(this).text().trim();
+    term.exec(command, {typing:true})
+});
+
